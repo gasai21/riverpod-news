e483f8045713ac2c06670bd0521fd834c03c2825
Gagah AP <gagahalfiandi3@gmail.com>
2025-10-30 22:32:35 +0700

    feat: add screen to all page

---
+++ lib/features/detail_news/screens/detail_news_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailNewsScreen extends StatelessWidget {
+  const DetailNewsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Detail News Screen")));
+  }
+}
